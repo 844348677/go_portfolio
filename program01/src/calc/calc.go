c383e227998efe1e7794cbdb3029fc80b521ca96
844348677 <844348677@qq.com>
2018-07-07 22:51:14 +0800

    2018/07/07 22:50

--- program01/src/calc/calc.go
+++ program01/src/calc/calc.go
@@ -1,5 +1,9 @@
 package calc
 
+import (
+	"fmt"
+)
+
 //不同目录　包名不一样
 //　调用不同包里的函数　包名.函数()
 //调用别的包的函数, 这个包函数的名字是小写　无法让别人调用，必须首字母大写
@@ -12,4 +16,9 @@ func Add(a, b int) int {
 }
 func Minus(a, b int) int {
 	return a - b
+}
+
+//导入包　先执行包里的　init函数
+func init() {
+	fmt.Println("this is calc init")
 }
--- program01/src/main.go
+++ program01/src/main.go
@@ -1,9 +1,18 @@
 package main
 
+//自动生成　ｐｋｇ　平台的静态库
+// go install
+//　ｂｉｎ　ｐｋｇ
+
 import (
 	"calc"
 	"fmt"
 )
+
+//先执行ｉｎｉｔ　再执行　ｍａｉｎ函数
+func init() {
+	fmt.Println("this is main init")
+}
 
 // go env 查看go相关的环境路径
 //　同一个目录，调用别的文件函数,直接调用即可，无需包名引用
