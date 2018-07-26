79df44da56e3f3d2bb8da50fecbbb966d3038ffe
844348677 <844348677@qq.com>
2018-07-26 20:03:21 +0800

    2018/07/26 20:00

--- building_microservices_with_go/src/rpc/client/client.go
+++ building_microservices_with_go/src/rpc/client/client.go
@@ -4,13 +4,31 @@ import (
 	"net/rpc"
 	"fmt"
 	"log"
+	"../contract"
 )
 
+// show how we need to setup rpc.client
 func CreateClient() *rpc.Client{
 	port := "8080"
+	// func Dial(network,address string) (*Client,error)
 	client,err := rpc.Dial("tcp",fmt.Sprintf("localhost:%v",port))
 	if err != nil {
 		log.Fatal("dialing: ",err)
 	}
 	return client
 }
+
+// we then use this returned connection to make a request to the server
+
+func PerformRequest(client *rpc.Client) contract.HelloWorldResponse{
+	args := &contract.HelloWorldRequest{Name:" World!"}
+	var reply contract.HelloWorldResponse
+
+	err := client.Call("HelloWorldHandler.HelloWorld",args,&reply)
+	if err != nil{
+		log.Fatal("error: ",err)
+	}
+	return reply
+}
+// Call : waits util the server sends a reply writing the response assuming there is no error to the reference of
+// our HelloWorldResponse passed to the method and
---
+++ building_microservices_with_go/src/rpc/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"./client"
+	"./server"
+)
+
+func main() {
+	go server.StartServer()
+
+	c := client.CreateClient()
+	defer c.Close()
+
+	reply := client.PerformRequest(c)
+	fmt.Println(reply.Message)
+}
