ebf40e64e49fbd6eb340446ddd4a9dd5e61dc512
844348677 <844348677@qq.com>
2018-07-27 01:27:53 +0800

    2018/07/27 01:30

---
+++ building_microservices_with_go/src/ch02/jsonp/jsonp.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type helloWorldResponse struct{
+	Message string `json:"message"`
+}
+
+func helloWorldHandler(w http.ResponseWriter,r *http.Request){
+	response := helloWorldResponse{Message:"Hello wold"}
+	data,err := json.Marshal(response)
+	if err != nil{
+		panic("Ooops")
+	}
+	// we are checking to see if there is a callback parameter in the query sting
+	// this would be provided by the client and indicates the function they expect to be called when the response is returned
+
+	callback := r.URL.Query().Get("callback")
+	if callback != ""{
+		// 书上代码有问题
+		r.Header.Add("Content-Type","application/javascript")
+		//w.Header().Add("Content-Type","application/javascript")
+		fmt.Fprintf(w,"%s (%s)",callback,string(data))
+	}else{
+		fmt.Fprint(w,string(data))
+	}
+
+}
+
+// Request
+// http://localhost:8080/helloworld?callback=hello
+// http://localhost:8080/helloworld
+
+func main(){
+	port := 8080
+	http.HandleFunc("/helloworld",helloWorldHandler)
+	log.Printf("server staring on port %v\n",8080)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v",port),nil))
+}
