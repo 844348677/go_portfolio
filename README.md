af5e13014c3a938b992fbe547b0cb2ae4e3d73d5
844348677 <844348677@qq.com>
2018-07-29 05:04:07 +0800

    2018/07/28 05:05

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ connect refuse   https://github.com/golang 被墙了　下载对应的包
 目前急需找机会把　.idea　文件夹删了
 
 需要把所有发牢骚的地方　转移到　备忘录网站上
+
+TODO 给文件夹　写一个简单的说明
+
+https://godoc.org/
--- building_microservices_with_go/src/ch04/data/mongostore.go
+++ building_microservices_with_go/src/ch04/data/mongostore.go
@@ -1,3 +1,39 @@
 package data
 
+import (
+	"gopkg.in/mgo.v2"
+	"time"
+)
 
+// MongoStore is a MongoDB data store which implements the Store interface
+type MongoStore struct {
+	session *mgo.Session
+}
+// 又是在哪找的　包
+// https://godoc.org/
+// https://godoc.org/labix.org/v2/mgo
+// go get gopkg.in/mgo.v2
+// http://labix.org/mgo
+// 待看文档
+
+func NewMongoStore(connection string) (*MongoStore,error){
+	sesseion,err := mgo.Dial(connection)
+	if err != nil{
+		return nil,err
+	}
+	return &MongoStore{session:sesseion},nil
+}
+
+// the Dial method attempts to connect ot he instance of MongoDB specified in the connection string
+
+
+// 这个方法不在这里
+func waitForDB(){
+	for i:=0 ; i<10 ;i++{
+		_,err := NewMongoStore("localhost")
+		if err == nil{
+			break
+		}
+		time.Sleep(1*time.Second)
+	}
+}
