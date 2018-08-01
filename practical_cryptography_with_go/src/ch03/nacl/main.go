6a6e58ded78144d1b74b67e2abc75953b6f758d1
844348677 <844348677@qq.com>
2018-08-02 05:06:21 +0800

    2018/08/02 05:00

--- practical_cryptography_with_go/src/ch03/nacl/main.go
+++ practical_cryptography_with_go/src/ch03/nacl/main.go
@@ -5,6 +5,8 @@ import (
 	"crypto/rand"
 	"fmt"
 	"bytes"
+	"errors"
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
 // It uses a 32-byte key and 24-byte nonces
@@ -56,3 +58,40 @@ func document_rand(){
 	fmt.Println(bytes.Equal(b, make([]byte, c)))
 	fmt.Println("b : ",b)
 }
+
+// https://nacl.cr.yp.to/index.html
+
+var (
+	ErrEncrypt = errors.New("secret: encryption failed")
+	ErrDecrypt = errors.New("secret: decryption failed")
+)
+
+// Encrypt generates a random nonce and encrypts the input using NaCl's secretbox package.
+// The nonce is prepended to the ciphertext.
+// A sealed message will the same size as the original message plus secretbox.Overhead bytes long.
+func Encrypt(key *[KeySize]byte, message []byte) ([]byte, error) {
+	// randomly generated nonces will be used;
+	nonce, err := GenerateNonce()
+	if err != nil {
+		return nil, ErrEncrypt
+	}
+
+	// The recipient will need some way of recovering the nonce, so it will be prepended to the message.
+
+	out := make([]byte, len(nonce))
+	copy(out, nonce[:])
+	fmt.Println("copy out : ",out)
+	out = secretbox.Seal(out, message, nonce, key)
+	return out, nil
+}
+
+// The length of messages is not hidden.
+
+
+// Decryption expects that the message contains a prepended nonce, and we verify this assumption by checking the length of the message.
+// A message that is too short to be a valid encryption message is dropped right away.
+
+
+
+
+
