f04cd1f462e02e6aaf439ecfadea03c79b38152e
844348677 <844348677@qq.com>
2018-08-20 13:32:36 +0800

    2018/08/20 13:30

--- blockchain02/remixd/addressSend.sol
+++ blockchain02/remixd/addressSend.sol
@@ -5,20 +5,20 @@ pragma  solidity ^0.4.20;
 
 
 // deploy 的时候　value　填空
-// 构造的不用传money过去
 // deploy 完毕的时候调用transfer的方法　填写value
 contract TransferTest {
     address AliceAddress = 0xb685ed019fc04b40454c56db1c2102083a5b47dc;
     
-    function transfer() {
-        AliceAddress.transfer(msg.value);
+    bool res;
+    function transfer() payable public returns (bool){
+        res = AliceAddress.send(msg.value);
+        return res;
     }
 }
 
 // send transfer
 // send 不会抛出异常　
 // send 相对于　transfer 语言更底层
-
 // send 执行有一些风险　　如果调用栈的　深度或者超过１０２４或ｇａｓ耗光，交易都会失败
 
 
