40eeb1964ab4315ea9d8b3125ade64a9c2119306
844348677 <844348677@qq.com>
2018-08-22 02:24:49 +0800

    2018/08/22

---
+++ c/ch02/prog1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Programming is fun. \n");
+
+	int sum;
+	sum = 50 + 25;
+	printf("The sum of 50 and 25 is %i\n",sum);
+
+	int value1,value2,sum2;
+
+	value1 = 50;
+	value2 = 25;
+
+	sum2 = value1 + value2;
+	printf ("the sum of %i and %i is %i \n",value1,value2,sum2);
+
+	int answer, result;
+
+	answer = 100;
+	result = answer - 10;
+	printf("the result is %i\n",result +5);
+
+	return 0;
+}
